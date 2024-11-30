0f4745d592e6640851d09faed60139fb2202d5a2
ReginaGo <0262761@up.edu.mx>
2024-11-29 21:49:35 -0600

    loco

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ app.set("view engine", "ejs");
 // );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://bien.onrender.com");
-  res.header("Access-Control-Allow-Methods", "POST, GET");
+  res.header("Access-Control-Allow-Origin", ['*']);
+  res.header("Access-Control-Allow-Methods", "POST, GET","PUT","DELETE");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
