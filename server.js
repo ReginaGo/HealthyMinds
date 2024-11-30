294771bdad34fdf93f89401cb42a04a69c3e84b9
ReginaGo <0262761@up.edu.mx>
2024-11-29 22:55:23 -0600

    maten a los cors

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.set("view engine", "ejs");
 // );
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://bien.onrender.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
